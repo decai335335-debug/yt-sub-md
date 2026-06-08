a851d29c7713a9723990ea70d8af24313166b799
15403 <1540386042@qq.com>
2026-06-08 22:43:57 +0800

    chore: add download.bat with preset output dir

---
+++ download.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d "E:/GitHubDownloads/kimi/yt-sub-md"
+echo [YouTube Subtitle Download]
+echo Output: E:/Obsidian/Ö÷²Ö¿â/11-subtitles/Youtube
+echo.
+python main.py --output "E:/Obsidian/Ö÷²Ö¿â/11-subtitles/Youtube"
+echo.
+echo Done.
+pause
